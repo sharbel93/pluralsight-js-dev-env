c337b5961f31bea65a4a1958447be4bb649a98e3
sharbel93 <sharbel.chris@gmail.com>
2019-07-02 19:07:46 +0300

    ESLint setup

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
 // var chalk = require('chalk');  // plugin to help choose the color for displaying a message
 import chalk from 'chalk'; // after adding a transpiler i.e babel
+/* eslint-disable no-console */
 console.log(chalk.green('Starting app in dev mode...'));
