618b4ca78bc5320eba2be808de17e045714be9de
sharbel93 <sharbel.chris@gmail.com>
2019-07-02 20:26:53 +0300

    DOM testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,21 @@
 import { expect } from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
    it('should pass', () => {
        expect(true).to.equal(true);
    });
+});
+
+// DOM Testing
+describe('index.html', () => {
+    it('should say server running', () => {
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+          const { JSDOM } = jsdom;
+          const dom = new JSDOM(index);
+            const h1 = dom.window.document.getElementsByTagName('h1')[0];
+            expect(h1.innerHTML).to.equal("Server Running");
+            dom.window.close();
+    });
 });
