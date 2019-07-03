cd41427e914ec71494d15d73de04e8b6a02d8e9d
sharbel93 <sharbel.chris@gmail.com>
2019-07-03 19:35:47 +0300

    how to fetch by using whatwg-fetch

--- src/index.test.js
+++ src/index.test.js
@@ -10,12 +10,12 @@ describe('Our first test', () => {
 
 // DOM Testing
 describe('index.html', () => {
-    it('should say server running', () => {
+    it('should have h1 that says Users', () => {
         const index = fs.readFileSync('./src/index.html', "utf-8");
           const { JSDOM } = jsdom;
           const dom = new JSDOM(index);
             const h1 = dom.window.document.getElementsByTagName('h1')[0];
-            expect(h1.innerHTML).to.equal("Server Running");
+            expect(h1.innerHTML).to.equal("Users");
             dom.window.close();
     });
 });
