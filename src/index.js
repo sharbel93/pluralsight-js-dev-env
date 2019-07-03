6753be5a4836073e647a2774b3f1f991b11a5404
sharbel93 <sharbel.chris@gmail.com>
2019-07-03 21:16:35 +0300

    manipulating data via json-server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 
 // import numeral from 'numeral';
-import {getUsers} from "./api/userApi";
+import {getUsers, deleteUser} from "./api/userApi";
 // /* eslint-disable no-console */
 // const courseValue = numeral(1000).format('$0,0.00');
 // // debugger;
@@ -25,4 +25,19 @@ getUsers().then(result => {
    });
 
    global.document.getElementById('users').innerHTML = usersBody;
-});
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+
+    // Must use array.from to create a real array from a DOM collection
+    // getElementsByClassname only returns an "array like" object
+    Array.from(deleteLinks, link => {
+        link.onclick = function (event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
+    });
