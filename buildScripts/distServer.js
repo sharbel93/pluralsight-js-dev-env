aa0ae92158dcc0aa1c33eaafbd06736f2f00e068
sharbel93 <sharbel.chris@gmail.com>
2019-07-05 08:13:46 +0300

    configure local /dist server

---
+++ buildScripts/distServer.js
@@ -0,0 +1,35 @@
+
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import compression from 'compression';
+/* eslint-disable no-console */
+
+const port = 3001;
+const app = express();
+
+
+app.use(compression());
+app.use(express.static("dist"));
+//root request endpoint for the server to index.html
+app.get('*', function(req,res) {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+
+app.get("/users", function(req, res) {
+    // Hard coding for simplicity. Pretend this hits a real database
+    res.json([
+        { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+        { id: 2, firstName: "Tammy", lastName: "Norton", email: "tno@yahoo.com" },
+        { id: 3, firstName: "Tina", lastName: "Lee", email: "lee.tina@hotmail.com" }
+    ]);
+});
+
+app.listen(port, function(err){
+    if(err) {
+        console.log(err);
+    } else {
+        open(`http://localhost:' + ${port}`);
+    }
+});
